1c5725a08f80e84bee0dc5cd3963ecc114eeb864
Ant <ananthajit5d@gmail.com>
2026-01-16 17:37:28 +0100

    evalRing

--- Cooper/src/EqReasoning.scala
+++ Cooper/src/EqReasoning.scala
@@ -60,7 +60,7 @@ object EqReasoning extends lisa.Main {
       
 
     def evalRing(using lib: library.type, proof: lib.Proof)(int: Expr[Ind]): (Biased, proof.ProofTacticJudgement) = {
-      x
+      
       var res : Biased = NRB(`0`)
       TacticSubproofWithResult[Biased]{
         int match {
@@ -75,8 +75,7 @@ object EqReasoning extends lisa.Main {
           case x + y => {
             val (vx, px) = evalRing(x)
             val (vy, py) = evalRing(y)
-            if(!px.isValid || !py.isValid) 
-            // early exit
+            if(!px.isValid || !py.isValid) // early exit
             then return (NRB(x), proof.InvalidProofTactic("evalRing failed!"))
             val (vsum, psum) = evalPlus(vx, vy)
             if(!psum.isValid) then return (NRB(x), proof.InvalidProofTactic("evalPlus failed!"))
@@ -89,30 +88,80 @@ object EqReasoning extends lisa.Main {
               (Seq(a, b), x + y === a + b)
             )
             thenHave(equalities |- x + y === uvsum) by RightSubstEq.withParameters(
-              Seq((x + y, uvsum)),
+              Seq((uvx + uvy, uvsum)),
               (Seq(a), x + y === a)
             )
             // WARNING: you may need val typs = SSet(x ∈ R, y ∈ R) ++ List(xeq, yeq, sumeq).map(l => getTypings(l.bot.left)).fold(Set())((x, y) => x ++ y)
             // TODO: Remove first set?
-
-            // var toCut = xeq.sRightHead
-            // equalities = equalities.excl(xeq.sRightHead)
-            // var tempAnte = equalities
-            // tempAnte = tempAnte.excl(xeq.sRightHead) ++ xeq.sLeft
-            // have(tempAnte |- x + y === uvsum) by Cut.withParameters(toCut)(xeq, lastStep)
-            // toCut = yeq.sRightHead 
-            // tempAnte = tempAnte.excl(yeq.sRightHead) ++ yeq.sLeft
-            // have(tempAnte |- x + y === uvsum) by Cut.withParameters(toCut)(yeq, lastStep)
-            // toCut = sumeq.sRightHead 
-            // tempAnte = tempAnte.excl(sumeq.sRightHead) ++ sumeq.sLeft
-            // have(tempAnte |- x + y === uvsum) by Cut.withParameters(toCut)(sumeq, lastStep)
-
+            var temp = evalRingCutHelper(xeq, x + y === uvsum, equalities)
+            have(temp._2)
+            temp = evalRingCutHelper(yeq, x + y === uvsum, temp._1)
+            have(temp._2)
+            temp = evalRingCutHelper(sumeq, x + y === uvsum, temp._1)
+            have(temp._2)
             // thenHave(x + y)
             res = RB(uvsum)
           }
+          // TODO: Check -(x) and x * y, the code is copypasted from the + case
+          case -(x) => {
+            val (vx, px) = evalRing(x)
+            if(!px.isValid) // early exit
+            then return (NRB(x), proof.InvalidProofTactic("evalRing failed!"))
+            val (vneg, pneg) = evalNeg(vx)
+            if(!pneg.isValid) then return (NRB(x), proof.InvalidProofTactic("evalNeg failed!"))
+            val (uvx, uvneg) = (vx, vneg).map[[t] =>> Expr[Ind]]([t] => x => unapply(x.asInstanceOf[Biased]))
+            val (xeq, negeq) = (have(px), have(pneg))
+            var equalities = SSet(xeq, negeq).map(_.bot.firstElemR)
+            have(equalities |- -(x) === -(x)) by Restate 
+            thenHave(equalities |- -(x) === uvx) by RightSubstEq.withParameters(
+              Seq((-(x), uvx)),
+              (Seq(a), -(x) === a)
+            )
+            thenHave(equalities |- -(x) === uvneg) by RightSubstEq.withParameters(
+              Seq((uvx, uvneg)),
+              (Seq(a), -(x) === a)
+            )
+            // WARNING: you may need val typs = SSet(x ∈ R, y ∈ R) ++ List(xeq, yeq, sumeq).map(l => getTypings(l.bot.left)).fold(Set())((x, y) => x ++ y)
+            // TODO: Remove first set?
+            var temp = evalRingCutHelper(xeq, -(x) === uvneg, equalities)
+            have(temp._2)
+            temp = evalRingCutHelper(negeq, -(x) === uvneg, temp._1)
+            have(temp._2)
+            res = RB(uvneg)
+          }
+          case x * y => {
+            val (vx, px) = evalRing(x)
+            val (vy, py) = evalRing(y)
+            if(!px.isValid || !py.isValid) // early exit
+            then return (NRB(x), proof.InvalidProofTactic("evalRing failed!"))
+            val (vmul, pmul) = evalMult(vx, vy)
+            if(!pmul.isValid) then return (NRB(x), proof.InvalidProofTactic("evalMult failed!"))
+            val (uvx, uvy, uvmul) = (vx, vy, vmul).map[[t] =>> Expr[Ind]]([t] => x => unapply(x.asInstanceOf[Biased]))
+            val (xeq, yeq, muleq) = (have(px), have(py), have(pmul))
+            var equalities = SSet(xeq, yeq, muleq).map(_.bot.firstElemR)
+            have(equalities |- x * y === x * y) by Restate 
+            thenHave(equalities |- x * y === uvx * uvy) by RightSubstEq.withParameters(
+              Seq((x, uvx), (y, uvy)),
+              (Seq(a, b), x * y === a * b)
+            )
+            thenHave(equalities |- x * y === uvmul) by RightSubstEq.withParameters(
+              Seq((uvx * uvy, uvmul)),
+              (Seq(a), x * y === a)
+            )
+            // WARNING: you may need val typs = SSet(x ∈ R, y ∈ R) ++ List(xeq, yeq, muleq).map(l => getTypings(l.bot.left)).fold(Set())((x, y) => x ++ y)
+            // TODO: Remove first set?
+            var temp = evalRingCutHelper(xeq, x * y === uvmul, equalities)
+            have(temp._2)
+            temp = evalRingCutHelper(yeq, x * y === uvmul, temp._1)
+            have(temp._2)
+            temp = evalRingCutHelper(muleq, x * y === uvmul, temp._1)
+            have(temp._2)
+            // thenHave(x + y)
+            res = RB(uvmul)
+          }
           // variables for last
-          case x : Variable[Ind] => ???
-
+          case x : Variable[Ind] => have(x === x) by Restate
+          case _ => return (NRB(int), proof.InvalidProofTactic("Not a Ring Element? evalRing Failed!"))
         }
       }(res)
     }
